06e1f8d84f76f570833ca04ecbeeb6105096ffaf
kimco2 <kymcoleman@gmail.com>
2023-02-12 14:14:49 +0800

    adding code and output

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # sql-challenge
+
+
+## Hello, this repository contains four folders:
+
+1. **data** - contains the csv files used for this project
+2. **data_modelling** - contains the entity relationship diagram
+3. **data_engineering** - contains the code used to create the tables and import the data
+4. **data_analysis** - contains the data analysis code for the eight questions,  along with the results for each qestion saved separately.
+
+Thanks
+
+
+Kim
---
+++ data_engineering/code_create_tables_import_data.sql
@@ -0,0 +1,93 @@
+-- Creating tables
+
+CREATE TABLE "titles" (
+    "title_id" varchar(5)   NOT NULL,
+    "title" varchar(18)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "employees" (
+    "emp_no" int   NOT NULL,
+    "emp_title_id" varchar(5)   NOT NULL,
+    "birth_date" date   NOT NULL,
+    "first_name" varchar(14)   NOT NULL,
+    "last_name" varchar(16)   NOT NULL,
+    "sex" varchar(1)   NOT NULL,
+    "hire_date" date   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "departments" (
+    "dept_no" varchar(4)   NOT NULL,
+    "dept_name" varchar(18)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" int   NOT NULL,
+    "salary" int   NOT NULL,
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar(4)   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
+);
+
+CREATE TABLE "dept_manager" (
+    "dept_no" varchar(4)   NOT NULL,
+    "emp_no" int   NOT NULL,
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
+);
+
+ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "titles" ("title_id");
+
+ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+ALTER TABLE "dept_emp" ADD CONSTRAINT "fk_dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "departments" ("dept_no");
+
+ALTER TABLE "dept_manager" ADD CONSTRAINT "fk_dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "employees" ("emp_no");
+
+-- Changing the date format
+SET datestyle TO 'ISO, MDY';
+
+-- Importing data into the tables from the relevant csv file
+COPY titles FROM 'C:\Users\kymco\Desktop\UWA_data_analytics\homework\sql-challenge\data\titles.csv'DELIMITER ',' CSV HEADER;
+COPY employees FROM 'C:\Users\kymco\Desktop\UWA_data_analytics\homework\sql-challenge\data\employees.csv'DELIMITER ',' CSV HEADER;
+COPY departments FROM 'C:\Users\kymco\Desktop\UWA_data_analytics\homework\sql-challenge\data\departments.csv'DELIMITER ',' CSV HEADER;
+COPY salaries FROM 'C:\Users\kymco\Desktop\UWA_data_analytics\homework\sql-challenge\data\salaries.csv'DELIMITER ',' CSV HEADER;
+COPY dept_emp FROM 'C:\Users\kymco\Desktop\UWA_data_analytics\homework\sql-challenge\data\dept_emp.csv'DELIMITER ',' CSV HEADER;
+COPY dept_manager FROM 'C:\Users\kymco\Desktop\UWA_data_analytics\homework\sql-challenge\data\dept_manager.csv'DELIMITER ',' CSV HEADER;
+
+-- Checking the data imported
+select * from titles;
+select * from employees;
+select * from departments;
+select * from salaries;
+select * from dept_emp;
+select * from dept_manager;
+
+
